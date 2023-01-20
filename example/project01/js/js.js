5d37fa769d3c7b5e17c686b28b418d0247b925f0
alsgkfka <118324424+alsgkfka@users.noreply.github.com>
2023-01-20 13:20:18 +0900

    .

--- example/project01/js/js.js
+++ example/project01/js/js.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
 
+    $('.gnbL li').mouseenter(function(){
+        
+        let i = $(this).index()
+        console.log(i)
+        $('.sub_menu>div').slideUp()
+        $('.sub'+(i+1)).slideDown()
+
+    });
+    $('header').mouseleave(function(){
+        $('.sub_menu>div').slideUp()
+
+    });
+
+    $('.gnbR li').mouseenter(function(){
+        
+        let i = $(this).index()
+        console.log(i)
+        $('.sub_menu>div').slideUp()
+        $('.sub'+(i+3)).slideDown()
+
+    });
+    $('header').mouseleave(function(){
+        $('.sub_menu>div').slideUp()
+
+    });
 
 })
